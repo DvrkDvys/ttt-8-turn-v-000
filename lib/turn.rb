d1b88c84290c684bc6aecbf302b3adc79fe47e13
Jordan Harris <jharris@wework.com>
2018-06-13 20:46:56 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,7 +45,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input) 
   if valid_move?(board, index) == false
-    puts "The current number is less than 20."
+    puts "Invalid"
   end
   # move(board, adjusted_input, user = "X")
 end
