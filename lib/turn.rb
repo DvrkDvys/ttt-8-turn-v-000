2c54e6473b1590e4bd53e272bad7678a65e4503c
Jordan Harris <jharris@wework.com>
2018-06-13 20:51:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,32 +46,15 @@ def turn(board)
   index = input_to_index(user_input) 
   if valid_move?(board, index) == false
     puts "Invalid"
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    return user_input
+    puts user_input
   end
   # move(board, adjusted_input, user = "X")
 end
 
 
-
-  # it 'validates the input correctly' do
-  #     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  #     allow($stdout).to receive(:puts)
-
-  #     expect(self).to receive(:gets).and_return("1")
-  #     expect(self).to receive(:valid_move?).with(board, 0).and_return(true)
-
-  #     turn(board)
-  #   end
-
-  #   it 'asks for input again after a failed validation' do
-  #     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-  #     allow($stdout).to receive(:puts)
-
-  #     expect(self).to receive(:gets).and_return("invalid")
-  #     expect(self).to receive(:gets).and_return("1")
-
-  #     turn(board)
-  #   end
 
 
 
